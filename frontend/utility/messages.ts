d3af78a7bc268797324428534e98f61ffab2de1e
ShachiniMEkala <>
2026-01-28 14:34:06 +0530

    Add commin messages

---
+++ frontend/utility/messages.ts
@@ -0,0 +1,9 @@
+export const messages = {
+    SUCCESSFULLY_DELETED: 'Successfully deleted!',
+    FAILED_TO_DELETE: 'Failed to delete',
+    SUCCESSFULLY_UPDATED: 'Successfully updated!',
+    FAILED_TO_UPDATE: 'Failed to update',
+    SUCCESSFULLY_CREATED: 'Successfully created!',
+    FAILED_TO_CREATE: 'Failed to create',
+    FAILED_TO_LOAD: 'Failed to load'
+}
