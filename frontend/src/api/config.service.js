59fcd3a6c21188f5f6f0c6bfab653a3cb4128a27
ShachiniMEkala <>
2026-01-28 21:01:04 +0530

    refactor the folder structure

--- frontend/src/api/config.service.js
+++ frontend/src/api/config.service.js
@@ -1,4 +1,4 @@
-import apiClient from './client';
+import apiClient from './core.service';
 
 export const configService = {
   getStatuses: () => apiClient.get('/statuses'),
--- frontend/src/api/project.service.js
+++ frontend/src/api/project.service.js
@@ -1,4 +1,4 @@
-import apiClient from './client';
+import apiClient from './core.service';
 
 export const projectService = {
   getAll: () => apiClient.get('/projects'),
